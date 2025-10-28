965833f49019380af58c3d724486cc73a826d1cd
alaa747474 <alaareda17878@gmail.com>
2025-10-28 21:33:10 +0300

    Refactor HomePage to use CustomScrollView and AppNavBar for improved layout and navigation

--- lib/modules/home/presentation/pages/home_page.dart
+++ lib/modules/home/presentation/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reckit_task/core/constants/app_sizes.dart';
 import 'package:reckit_task/core/di/service_locator.dart';
-import 'package:reckit_task/core/responsive/responsive_layout.dart';
+import 'package:reckit_task/core/widgets/app_nav_bar.dart';
+import 'package:reckit_task/modules/home/presentation/widgets/items_header_row.dart';
 import 'package:reckit_task/modules/trips/presentation/cubit/trips_cubit.dart';
 import 'package:reckit_task/modules/trips/presentation/widgets/trip_card.dart';
 
@@ -13,43 +15,48 @@ class HomePage extends StatelessWidget {
     return BlocProvider(
       create: (context) => sl<TripsCubit>()..getTrips(),
       child: Scaffold(
-        body: ResponsiveLayout(
-          mobile: BlocBuilder<TripsCubit, TripsState>(
-            builder: (context, state) {
-              if (state is TripsLoaded) {
-                return ListView.builder(
-                  itemCount: state.trips.length,
-                  itemBuilder: (context, index) {
-                    final trip = state.trips[index];
-                    return TripCard(trip: trip);
-                  },
-                );
-              }
-              return Text("adsafnlasjknakl");
-            },
-          ),
-          tablet: SizedBox(),
-          desktop: BlocBuilder<TripsCubit, TripsState>(
-            builder: (context, state) {
-              if (state is TripsLoaded) {
-                return GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    childAspectRatio: 3 / 2,
+        appBar: AppNavBar(),
+        body: BlocBuilder<TripsCubit, TripsState>(
+          builder: (context, state) {
+            if (state is TripsLoaded) {
+              return CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(child: ItemsHeaderRow()),
+
+                  SliverPadding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: AppSizes.horizontalPadding,
+                      vertical: AppSizes.verticalPadding,
+                    ),
+                    sliver: SliverGrid(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: (MediaQuery.of(context).size.width ~/ 270).toInt(),
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        childAspectRatio: _getAspectRatio(context),
+                      ),
+                      delegate: SliverChildBuilderDelegate((context, index) {
+                        final trip = state.trips[index];
+                        return TripCard(trip: trip);
+                      }, childCount: state.trips.length),
+                    ),
                   ),
-                  itemCount: state.trips.length,
-                  itemBuilder: (context, index) {
-                    final trip = state.trips[index];
-                    return TripCard(trip: trip);
-                  },
-                 
-                );
-              }
-              return Text("adsafnlasjknakl");
-            },
-          ),
+                ],
+              );
+            }
+
+            return const Center(child: CircularProgressIndicator());
+          },
         ),
       ),
     );
+  }
+
+  double _getAspectRatio(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final crossAxisCount = (width ~/ 270).toInt();
+    const itemHeight = 360;
+    final itemWidth = width / crossAxisCount;
+    return itemWidth / itemHeight;
   }
 }
