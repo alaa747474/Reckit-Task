09e79f3e52593a1edc9045127f56e1d6df9c893b
alaa747474 <alaareda17878@gmail.com>
2025-10-28 21:32:33 +0300

    Add selected and unselected colors to CustomColors theme extension

--- lib/core/config/theme/custom_colors.dart
+++ lib/core/config/theme/custom_colors.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 /// Custom color extension for theme-specific colors
 class CustomColors extends ThemeExtension<CustomColors> {
-  final Color? warningColor, statusCardColor;
+  final Color? warningColor, statusCardColor,selectedColor,unselectedColor;
 
-  const CustomColors({this.warningColor, this.statusCardColor});
+  const CustomColors({this.warningColor, this.statusCardColor, this.selectedColor,this.unselectedColor});
 
   @override
   CustomColors copyWith({Color? borderColor, Color? statusCardColor}) {
     return CustomColors(
       warningColor: borderColor ?? warningColor,
       statusCardColor: statusCardColor ?? statusCardColor,
+      selectedColor: selectedColor ?? selectedColor,
+      unselectedColor: unselectedColor ?? unselectedColor,
     );
   }
 
@@ -20,6 +22,8 @@ class CustomColors extends ThemeExtension<CustomColors> {
     return CustomColors(
       warningColor: Color.lerp(warningColor, other.warningColor, t)!,
       statusCardColor: Color.lerp(statusCardColor, other.statusCardColor, t)!,
+      selectedColor: Color.lerp(selectedColor, other.selectedColor, t)!,
+      unselectedColor: Color.lerp(unselectedColor, other.unselectedColor, t)!,
     );
   }
 
@@ -27,5 +31,7 @@ class CustomColors extends ThemeExtension<CustomColors> {
   static const dark = CustomColors(
     warningColor: Color(0xFFFFC268),
     statusCardColor: Color(0xFFC25F30),
+    selectedColor: Colors.white,
+    unselectedColor: Color(0xFF999999),
   );
 }
