8f07b42079cd613052175f3104a922a21d656e16
alaa747474 <alaareda17878@gmail.com>
2025-10-28 21:32:14 +0300

    Add new SVG icons for logo, menu, plus, and sliders; update IconsManager to include them

--- lib/core/constants/assets/icons_manager.dart
+++ lib/core/constants/assets/icons_manager.dart
@@ -7,6 +7,10 @@ class IconsManager {
   static const String notification = '$_base/notification.svg';
   static const String setting = '$_base/setting.svg';
   static const String downArrow = '$_base/down-arrow.svg';
+  static const String logo = '$_base/logo.svg';
+  static const String sliders= '$_base/sliders.svg';
+  static const String plus= '$_base/plus.svg';
+  static const String menu= '$_base/menu.svg';
 
 
 
