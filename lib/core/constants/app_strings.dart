8439215876b811be5ffa98108c34fa27ed73eb96
alaa747474 <alaareda17878@gmail.com>
2025-10-28 19:30:13 +0300

    Add AppStrings class for centralized string management

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,13 @@
+class AppStrings {
+  static const appName = 'Reckit Task';
+  static const home = 'Home';
+  static const trips = 'Trips';
+  static const unfinishedTasks = 'unfinished tasks';
+  static const items = 'Items';
+  static const pricing = 'Pricing';
+  static const info = 'Info';
+  static const tasks = 'Tasks';
+  static const analytics = 'Analytics';
+  static const addItem = 'Add a New Item';
+  static const nights = 'Nights';
+}
