23e2092fc0249a05f8f74462bc45a71d88a8c46c
alaa747474 <alaareda17878@gmail.com>
2025-10-28 21:32:24 +0300

    Refactor app theme configuration and update web metadata; replace favicon image

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:reckit_task/core/config/app_initializer.dart';
-import 'package:reckit_task/core/theme/app_theme.dart';
+import 'package:reckit_task/core/config/theme/app_theme.dart';
+import 'package:reckit_task/core/constants/app_strings.dart';
 import 'package:reckit_task/modules/home/presentation/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-   await AppInitializer().init();
+  await AppInitializer().init();
   runApp(const MyApp());
 }
+
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,10 +18,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       theme: AppTheme.getTheme(context),
-       home:  HomePage(),
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+      title: AppStrings.appName,
+      theme: AppTheme.darkTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
+      home: HomePage(),
     );
   }
 }
-
-
