747df7760bcfe75bef73e86483a7a8f717ab8ac0
alaa747474 <alaareda17878@gmail.com>
2025-10-28 19:28:54 +0300

    Add SVG icons for calendar, horizontal dots, notification, and settings

---
+++ lib/core/constants/assets/icons_manager.dart
@@ -0,0 +1,12 @@
+class IconsManager {
+  static const String _base = 'assets/icons';
+
+  // Splash / Logos
+  static const String horizontalDots = '$_base/horizontal-dots.svg';
+  static const String calendar = '$_base/calendar.svg';
+  static const String notification = '$_base/notification.svg';
+  static const String setting = '$_base/setting.svg';
+
+
+
+}
